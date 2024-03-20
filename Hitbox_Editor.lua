3b1d147667588ee5c7b6487b583fa263fd2eebc4
kaiiboraka <kaiiboraka@gmail.com>
2024-03-20 14:43:17 -0600

    v44

--- Hitbox_Editor.lua
+++ Hitbox_Editor.lua
@@ -44,9 +44,9 @@ function dev(str)
 end
 
 function dev(str, tag)
-    -- if type(str) == "table" then
-        -- str = ''..table_to_string(str)
-    -- end
+    if type(str) == "table" then
+        str = ''..table_to_string(str)
+    end
     if tag == 'verbose' then
         -- print(str)
     elseif tag == 'error' then
@@ -56,6 +56,35 @@ function dev(str, tag)
     end
 end
 
+UTILS = {
+drawRealImg=(function(ctx, SIZE)
+    local sprite = app.sprite
+    local img = Image(sprite.width, sprite.height, sprite.colorMode)
+    img:drawSprite(sprite, app.site.frameNumber)
+    ctx:drawImage(img,0,0,sprite.width,sprite.height,0,0,SIZE,SIZE)
+end),
+getSelection = (function(startPt,endPt,SIZE,coordsType)
+    local x = math.min(startPt.x, endPt.x)
+    local y = math.min(startPt.y, endPt.y)
+    local w = math.abs(endPt.x - startPt.x)
+    local h = math.abs(endPt.y - startPt.y)
+    if coordsType == 'real' then
+        return Rectangle{ x=x, y=y, width=w, height=h }
+    end
+    local sprite = app.sprite
+    local drawScaleX = SIZE/sprite.width
+    local drawScaleY = SIZE/sprite.height
+    x=math.floor(x/drawScaleX)
+    y=math.floor(y/drawScaleY)
+    w=math.floor(w/drawScaleX)
+    h=math.floor(h/drawScaleY)
+    if coordsType == 'canvas' then
+        return Rectangle{ x=x*drawScaleX, y=y*drawScaleY, width=w*drawScaleX, height=h*drawScaleY }
+    elseif coordsType == 'sprite' then
+        return Rectangle{ x=x, y=y, width=w, height=h }
+    end
+end),
+}
 function createHitBox()
     local dialog = Dialog("New Hitbox")
     local sprite = app.sprite
@@ -64,40 +93,24 @@ function createHitBox()
     local drawScaleY = SIZE/sprite.height
     local startPt = {x=(sprite.width/4)*drawScaleX, y=(sprite.height/4)*drawScaleY}
     local endPt = {x=(3*sprite.width/4)*drawScaleX, y=(3*sprite.height/4)*drawScaleY}
-    local selecting = false
-    function getSelection(coordsType)
-        local x = math.min(startPt.x, endPt.x)
-        local y = math.min(startPt.y, endPt.y)
-        local w = math.abs(endPt.x - startPt.x)
-        local h = math.abs(endPt.y - startPt.y)
-        if coordsType == 'real' then
-            return Rectangle{ x=x, y=y, width=w, height=h }
-        end
-        x=math.floor(x/drawScaleX)
-        y=math.floor(y/drawScaleY)
-        w=math.floor(w/drawScaleX)
-        h=math.floor(h/drawScaleY)
-        if coordsType == 'canvas' then
-            return Rectangle{ x=x*drawScaleX, y=y*drawScaleY, width=w*drawScaleX, height=h*drawScaleY }
-        elseif coordsType == 'sprite' then
-            return Rectangle{ x=x, y=y, width=w, height=h }
-        end
-    end
+    local selecting = false    
     function fillBox(ev)
         local site = app.site
         local layer = app.activeLayer
         local newHitbox = sprite:newLayer()
         newHitbox.parent = hitboxGroup
         newHitbox.name = dialog.data.name
+        hitboxGroup.isEditable = true
         -- newHitbox.isContinuous = true -- use continuousness for iterpolating
-        sprite.selection:select(getSelection('sprite'))
+        sprite.selection:select(UTILS.getSelection(startPt,endPt,SIZE,'sprite'))
         local colorfg = app.fgColor
         app.fgColor = Color{r=dialog.data.color.red, g=dialog.data.color.green, b=dialog.data.color.blue, a=64}
         app.command.Fill{ }
         app.fgColor = colorfg
         sprite.selection:deselect()
-        app.site = site
         app.activeLayer = layer
+        app.site = site
+        hitboxGroup.isEditable = false
         dialog:close()
     end
     dialog:label{text="Hitbox name:"}:entry{id='name', text='Hitbox #'..(#hitboxGroup.layers+1), }:separator{}
@@ -119,30 +132,21 @@ function createHitBox()
         end),
         onpaint=(function(ev) 
             local ctx = ev.context
-            local sprite = app.sprite
-            local img = Image(sprite.width, sprite.height, sprite.colorMode)
-            img:drawSprite(sprite, app.site.frameNumber)
-            ctx:drawImage(img,0,0,sprite.width,sprite.height,0,0,SIZE,SIZE)
+            UTILS.drawRealImg(ctx,SIZE)
             ctx.color = dialog.data.color
             ctx.color = Color{r=ctx.color.red, g=ctx.color.green, b=ctx.color.blue, a=64};
-            ctx:fillRect(getSelection('canvas'))
+            ctx:fillRect(UTILS.getSelection(startPt,endPt,SIZE,'canvas'))
         end)
     }
     dialog:separator{}:label{text="Color:"}:color{ id='color',
     color=Color{r=255, g=0, b=0, a=255},
     onchange=(function(ev)dialog:repaint() end)}
     :separator()
-    :button{ id="ok", text="ACCEPT", onclick=fillBox }
+    :button{ id="ok", text="ACCEPT", onclick=fillBox, focus=true }  
     :show{ wait=false }
 end
 
--- Open dialog, ask user for two colors
 function loadData()
-    -- sprite.events:on('move',
-    --     function(ev)
-    --         print('The sprite has changed '..table_to_string(ev))
-    --     end
-    -- )
     local site = app.site
     local layer = app.activeLayer
     hitboxData = {}
@@ -160,69 +164,56 @@ function loadData()
         hitboxGroup.name = 'Hitboxes';
         -- dev('created group')
     end
-    
+    hitboxGroup.isEditable = false
     for i,layer in ipairs(hitboxGroup.layers) do
         hitboxData[layer.name] = layer
     end
     
-    app.site = site
     app.activeLayer = layer
-    -- app.alert("Selection: "..tostring(selection.bounds))
-    -- app.editor:askPoint{
-    --     title='wow',
-    --     onclick=function(ev)
-    --         print(tostring(ev.x).." "..tostring(ev.y).." "..tostring(event))
-    --     end
-    -- }
+    app.site = site
+    app.refresh()
 end
+
+function toggleVisibility()
+    local site = app.site
+    local layer = app.activeLayer
+    if hitboxGroup.isVisible then
+        hitboxGroup.isCollapsed = true
+        hitboxGroup.isVisible = false                    
+        for i = 1,#hitboxGroup.layers do
+            hitboxGroup.layers[i].isVisible = false
+        end
+        menu:modify{id='vis',text='--'}
+        app.refresh()
+    else            
+        hitboxGroup.isVisible = true
+        hitboxGroup.isExpanded = true                    
+        for i = 1,#hitboxGroup.layers do
+            hitboxGroup.layers[i].isVisible = true
+        end
+        menu:modify{id='vis',text='<>'}
+        app.refresh()
+    end 
+    app.activeLayer = layer
+    app.site = site
+end
+
 function noOP()
-end
-function queueEvent(event, callback)
-    queueEvent(event, callback, noOP)
-end
-function queueEvent(event, callback, cancel)
-    if eventQueue[event] then
-        eventQueue[event].cancel()
-    end
-    eventQueue[event] = {callback=callback, cancel=cancel}
 end
 
 function createMenu()
     if menu then
-        menu.close()
+        menu:close()
     end
-    local function listener(type, ev)
-        if eventQueue[type] then
-            eventQueue[type].callback(ev)
-            eventQueue[type] = nil
-        end
-        dev(type.." Event: "..table_to_string(ev), 'verbose')
-    end
-    local function beforelistener(ev)
-        listener('before', ev)
-    end
-    local function afterlistener(ev)
-        listener('after', ev)
-    end
-    local function changelistener(ev)
-        listener('change', ev)
-    end
-    local sprite = app.sprite
-    app.events:on('beforecommand', beforelistener)
-    sprite.events:on('change', changelistener)
-    app.events:on('aftercommand', afterlistener)
-    menu = Dialog{title="Hitbox Editor Toolbar", onclose=(function() 
-        app.events:off(beforelistener)
-        sprite.events:off(changelistener)
-        app.events:off(afterlistener)
-        sprite:deleteLayer(hitboxGroup)
-        menu = nil
-     end)}:button{text="+",onclick=createHitBox}:show{wait=false}
-end
-
-do
-    local hitboxData, hitboxGroup, menu
-    eventQueue = { before=nil, after=nil, change=nil }
     loadData()
-    createMenu()
+    menu = Dialog{title="Hitbox Toolbar", onclose=(function()
+        app.sprite:deleteLayer(hitboxGroup)
+        menu = nil
+     end)}:button{text="+◻",onclick=createHitBox}
+    :button{text='++',onclick=noOP}
+    :button{text="◻?",onclick=noOP}
+    :button{id='vis',text='<>',onclick=toggleVisibility}
+    :button{text='...',onclick=noOP}:show{wait=false}
 end
+local hitboxData, hitboxGroup, menu
+createMenu()
